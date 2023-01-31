81ae96d82cc26ccbea7fe075fa430a615e810569
anhtuan2212 <lutan2212@gmail.com>
2023-01-31 17:09:20 +0700

    đã code xong giao diện nhân sự và các thông báo

---
+++ public/assets/js/core/personnel.js
@@ -0,0 +1,57 @@
+function onDelete(){
+    const swalWithBootstrapButtons = Swal.mixin({
+    customClass: {
+        confirmButton: 'btn btn-success',
+        cancelButton: 'btn btn-danger'
+    },
+    buttonsStyling: false
+})
+
+swalWithBootstrapButtons.fire({
+    title: 'Bạn muốn xóa ?',
+    text: "Sau khi xóa không thể khôi phục !",
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonText: 'Xóa',
+    cancelButtonText: 'Không',
+    reverseButtons: true
+}).then((result) => {
+    if (result.isConfirmed) {
+        swalWithBootstrapButtons.fire(
+            'Thành Công !',
+            'Nhân sự của bạn đã bị xóa.',
+            'success'
+        )
+    } else if (
+        /* Read more about handling dismissals below */
+        result.dismiss === Swal.DismissReason.cancel
+    ) {
+        swalWithBootstrapButtons.fire(
+            'Đã Hủy !',
+            'Tác vụ xóa đã được hủy.',
+            'error'
+        )
+    }
+})
+}
+function onAlertSuccess(){
+    Swal.fire({
+        position: 'top-end',
+        width:400,
+        icon: 'success',
+        title: 'Thành Công !',
+        showConfirmButton: false,
+        timer: 1000
+      })
+}
+
+function onAlertError(){
+    Swal.fire({
+        position: 'top-end',
+        width:400,
+        icon: 'error',
+        title: 'Thất Bại !',
+        showConfirmButton: false,
+        timer: 1000
+      })
+}
--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,7 @@ use App\Http\Controllers\LoginController;
 use App\Http\Controllers\UserProfileController;
 use App\Http\Controllers\ResetPassword;
 use App\Http\Controllers\ChangePassword;
+use App\Http\Controllers\PersonnelController;
 
 Route::get('/', function () {
 	return redirect('/dashboard');
@@ -36,6 +37,8 @@ Route::post('/reset-password', [ResetPassword::class, 'send'])->middleware('gues
 Route::get('/change-password', [ChangePassword::class, 'show'])->middleware('guest')->name('change-password');
 Route::post('/change-password', [ChangePassword::class, 'update'])->middleware('guest')->name('change.perform');
 Route::get('/dashboard', [HomeController::class, 'index'])->name('home')->middleware('auth');
+Route::get('/personnel', [PersonnelController::class, 'show'])->name('personnel')->middleware('auth');
+
 Route::group(['middleware' => 'auth'], function () {
 	Route::get('/virtual-reality', [PageController::class, 'vr'])->name('virtual-reality');
 	Route::get('/rtl', [PageController::class, 'rtl'])->name('rtl');
